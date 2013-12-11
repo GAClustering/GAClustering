82bbe6262b423e41c0788297f40886e541c6fe3e
pongga11 <ratipong.pool@gmail.com>
2013-12-11 16:23:36 +0700

    Write cluster result to file

--- AdaptiveGAClusteringParallel/src/gaclustering/Population.java
+++ AdaptiveGAClusteringParallel/src/gaclustering/Population.java
@@ -1,6 +1,9 @@
 package gaclustering;
+
+import au.com.bytecode.opencsv.CSVWriter;
 import com.utils.Purity;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,64 +11,64 @@ import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Population{
-  private Double Fitness;
-  private int Chromosome[];
-  
-  ArrayList<Integer> dataNode;
-  private ArrayList<ArrayList<Integer>> clusterResult;
-  public Population(){
+public class Population {
 
-            Chromosome = new int[Const.CHROMOSOME_LENGTH];
-            Fitness = 0.0;
-            clusterResult= new ArrayList<>();
-  }
-         
+    private Double Fitness;
+    private int Chromosome[];
 
-  public void setFitness(Double Value){
-          this.Fitness = Value;
-      }
-  
-  public Double getFitness(){
-          return Fitness;
-      }
-  
-  private boolean isInvalid(){
+    ArrayList<Integer> dataNode;
+    private ArrayList<ArrayList<Integer>> clusterResult;
+
+    public Population() {
+
+        Chromosome = new int[Const.CHROMOSOME_LENGTH];
+        Fitness = 0.0;
+        clusterResult = new ArrayList<>();
+    }
+
+    public void setFitness(Double Value) {
+        this.Fitness = Value;
+    }
+
+    public Double getFitness() {
+        return Fitness;
+    }
+
+    private boolean isInvalid() {
         // Check duplicate
         boolean dup = false;
-        for (int i=0; i< Chromosome.length;i++) {
-            for (int j=0; j< Chromosome.length;j++) {
-                if ( i!=j && Chromosome[i] == Chromosome[j]){
+        for (int i = 0; i < Chromosome.length; i++) {
+            for (int j = 0; j < Chromosome.length; j++) {
+                if (i != j && Chromosome[i] == Chromosome[j]) {
                     dup = true;
                 }
             }
         }
-      
-      return dup;
-  }
 
-  public Double calFitness(){
-      
+        return dup;
+    }
+
+    public Double calFitness() {
+
         if (isInvalid()) {
             //System.out.println("Invalid!!");
-            
+
             this.setChromosome(Gencommand());
-            
+
         }
-      
-      
+
         Double fitness = 0.0;
 
         clustering c = new clustering();
         c.setDissimilarity(Const.SimilarityMatrix);
 
         ArrayList<ArrayList<Integer>> clusterMedroid = new ArrayList<>();
-        int clusterNo=0;
+        int clusterNo = 0;
         ArrayList<Integer> d = new ArrayList<>();
         for (int i = 0; i < Chromosome.length; i++) {
             d.add(Chromosome[i]);
 
-            if ((i+1) % Const.MEDROID_LENGTH == 0){
+            if ((i + 1) % Const.MEDROID_LENGTH == 0) {
                 clusterMedroid.add(clusterNo, d);
 
                 clusterNo++;
@@ -75,43 +78,25 @@ public class Population{
 
         fitness = c.findCluster(clusterMedroid);
 
-
-                
         setClusterResult(clusterMedroid);
-         return fitness;
+        return fitness;
     }
-  
-  public void printResult(){
-//        dataNode = new ArrayList<Integer>();
-//
-//        clustering c = new clustering();
-//        c.setDissimilarity(Const.SimilarityMatrix);
-//        ArrayList<ArrayList<Integer>> clusterMedroid = new ArrayList<>();
-//        int clusterNo=0;
-//        ArrayList<Integer> d = new ArrayList<>();
-//        for (int i = 0; i < Chromosome.length; i++) {
-//            d.add(Chromosome[i]);
-//            if ((i+1) % Const.MEDROID_LENGTH == 0){
-//                clusterMedroid.add(clusterNo, d);
-//                clusterNo++;
-//                d = new ArrayList<>();
-//            }
-//        }
-//
-//        c.printResult(clusterMedroid);
-   
-        for( int i = 0 ; i< getClusterResult().size(); i++){
-            System.out.println("\n Output for : " +( i+1)+ " th, n = " + getClusterResult().get(i).size() );
-            for (int j=0; j< getClusterResult().get(i).size(); j++){
+
+    public void printResult() throws IOException {
+
+        CSVWriter writer = new CSVWriter(new FileWriter("output_" + this.getFitness() + ".csv"));
+        for (int i = 0; i < getClusterResult().size(); i++) {
+            System.out.println("\n Output for : " + (i + 1) + " th, n = " + getClusterResult().get(i).size());
+            for (int j = 0; j < getClusterResult().get(i).size(); j++) {
                 System.out.print(getClusterResult().get(i).get(j) + ", ");
             }
+            writer.writeNext(getClusterResult().get(i).toString());
             System.out.println();
-            
-        }
-        
 
-      
-  }
+        }
+        writer.close();
+
+    }
 
     /**
      * @return the Chromosome
@@ -126,24 +111,23 @@ public class Population{
     public void setChromosome(int[] Chromosome) {
         this.Chromosome = Chromosome;
     }
-    
-    private int[] Gencommand(){
-        
+
+    private int[] Gencommand() {
+
         //System.out.println("INVALID CHROMOSOME IS REPLACED!!!");
-        
         int tmpChromosome[] = new int[Const.CHROMOSOME_LENGTH];
         ArrayList<Integer> DataSet = new ArrayList<Integer>();
-        for (int i = 1; i<= Const.TOTAL_RECORDS;i++){
+        for (int i = 1; i <= Const.TOTAL_RECORDS; i++) {
             DataSet.add(i);
         }
         Random rand = new Random();
-        for (int i=0; i<Const.CHROMOSOME_LENGTH; i++ ){
+        for (int i = 0; i < Const.CHROMOSOME_LENGTH; i++) {
             int nextRand = rand.nextInt(DataSet.size());
             tmpChromosome[i] = DataSet.remove(nextRand);
         }
-        
+
         return tmpChromosome;
-        
+
     }
 
     /**
@@ -159,5 +143,5 @@ public class Population{
     public void setClusterResult(ArrayList<ArrayList<Integer>> clusterResult) {
         this.clusterResult = clusterResult;
     }
-  
+
 }
--- AdaptiveGAClusteringParallel/src/gaclustering/Run.java
+++ AdaptiveGAClusteringParallel/src/gaclustering/Run.java
@@ -1,4 +1,3 @@
-
 package gaclustering;
 
 import com.utils.Purity;
@@ -16,142 +15,122 @@ import javax.swing.JOptionPane;
  */
 public class Run extends Thread {
 
-    public Run(){
-    
+    public Run() {
+
     }
 
-        @Override
+    @Override
     public void run() {
 
-            AGAC.progessBar.setMinimum(0);
-            AGAC.progessBar.setMaximum(Const.SLACK_GEN);
-            Genetic aGenetic = new Genetic();
-            Population bestPopulation = new Population();
-            aGenetic.GenPop();
-            aGenetic.printAllChromosomes();
+        AGAC.progessBar.setMinimum(0);
+        AGAC.progessBar.setMaximum(Const.SLACK_GEN);
+        Genetic aGenetic = new Genetic();
+        Population bestPopulation = new Population();
+        aGenetic.GenPop();
+        aGenetic.printAllChromosomes();
 
-            
-            int current_slack =0;
-            int total_slack = 0;
-            int current_mode = 1;
+        int current_slack = 0;
+        int total_slack = 0;
+        int current_mode = 1;
 
-            AGAC.outputText.append("Mode#: " + current_mode + "\n");
-            AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length()-1);
-                    
-            aGenetic.Execute();
-            bestPopulation.setChromosome(aGenetic.bestPop().getChromosome());
-            bestPopulation.setFitness(aGenetic.bestPop().getFitness());
-            
-            
-            java.util.Date start_time= new java.util.Date();
-            
-            
-            while ( total_slack < (Const.SLACK_GEN * 4) ) {
+        AGAC.outputText.append("Mode#: " + current_mode + "\n");
+        AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length() - 1);
+
+        aGenetic.Execute();
+        bestPopulation.setChromosome(aGenetic.bestPop().getChromosome());
+        bestPopulation.setFitness(aGenetic.bestPop().getFitness());
+
+        java.util.Date start_time = new java.util.Date();
+
+        while (total_slack < (Const.SLACK_GEN * 4)) {
             //while ( bestPopulation.getFitness() <= 4020) {
-                
-                if (current_mode == 1) {
-                    aGenetic.Reproduction();
-                }
-                else if (current_mode == 2) {
-                    aGenetic.Reproduction2();
-                }
-                else {
-                    aGenetic.Reproduction3();
 
-                }
-                
-                
-                aGenetic.Execute();
-                
-                
-                if ( aGenetic.bestPop().getFitness() > bestPopulation.getFitness()) {
- 
-                    current_slack = 0;
-                    
-                    
-                    bestPopulation.setChromosome(aGenetic.bestPop().getChromosome());
-                    bestPopulation.setFitness(aGenetic.bestPop().getFitness());
-                    bestPopulation.setClusterResult(aGenetic.bestPop().getClusterResult());
-                    AGAC.outputText.append("Best Chrosome = " 
-                            + bestPopulation.getFitness() + " = " 
-                            + Arrays.toString(bestPopulation.getChromosome()) + "  (mode: " + current_mode + ")\n");
-                    AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length()-1);
+            if (current_mode == 1) {
+                aGenetic.Reproduction();
+            } else if (current_mode == 2) {
+                aGenetic.Reproduction2();
+            } else {
+                aGenetic.Reproduction3();
 
-                    System.out.println( "Best Chrosome = " + bestPopulation.getFitness() + " = " + Arrays.toString(bestPopulation.getChromosome()));
-
-
-                }
-                else {
-                    current_slack++;
-                    
-                }
-
-                if ( current_slack >= Const.SLACK_GEN ) {
-                    
-                    current_mode++;
-                    if (current_mode > 3) {
-                        current_mode = 1;
-                    }
-                    
-                    total_slack += current_slack;
-                    current_slack = 0;
-                    //aGenetic.printAllChromosomes();
-                    System.out.println("Move to mode# " + current_mode);
-                    AGAC.outputText.append("Move to mode# " + current_mode + "\n");
-                    AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length()-1);
-                }
-                
-                
-                AGAC.progessBar.setValue(current_slack);
-                AGAC.progessBar.repaint();
-                
-                
-                
             }
-            
-            
-          bestPopulation.printResult();
-          
-          
-        // Calculate cluster purity
-        Purity P = new Purity();
-        try {        
-            System.out.println("Cluster purity = " + P.callPurity(bestPopulation.getClusterResult()));
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(clustering.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(clustering.class.getName()).log(Level.SEVERE, null, ex);
+
+            aGenetic.Execute();
+
+            if (aGenetic.bestPop().getFitness() > bestPopulation.getFitness()) {
+
+                current_slack = 0;
+
+                bestPopulation.setChromosome(aGenetic.bestPop().getChromosome());
+                bestPopulation.setFitness(aGenetic.bestPop().getFitness());
+                bestPopulation.setClusterResult(aGenetic.bestPop().getClusterResult());
+                AGAC.outputText.append("Best Chrosome = "
+                        + bestPopulation.getFitness() + " = "
+                        + Arrays.toString(bestPopulation.getChromosome()) + "  (mode: " + current_mode + ")\n");
+                AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length() - 1);
+
+                System.out.println("Best Chrosome = " + bestPopulation.getFitness() + " = " + Arrays.toString(bestPopulation.getChromosome()));
+
+            } else {
+                current_slack++;
+
+            }
+
+            if (current_slack >= Const.SLACK_GEN) {
+
+                current_mode++;
+                if (current_mode > 3) {
+                    current_mode = 1;
+                }
+
+                total_slack += current_slack;
+                current_slack = 0;
+                //aGenetic.printAllChromosomes();
+                System.out.println("Move to mode# " + current_mode);
+                AGAC.outputText.append("Move to mode# " + current_mode + "\n");
+                AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length() - 1);
+            }
+
+            AGAC.progessBar.setValue(current_slack);
+            AGAC.progessBar.repaint();
+
         }
-          
-          
-          
-          System.out.println( "Fitness " + bestPopulation.getFitness());
-          
-          
+        
+        try {
+            bestPopulation.printResult();
+
+        } catch (IOException ex) {
+            Logger.getLogger(Run.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // Calculate cluster purity
+//        Purity P = new Purity();
+//        try {        
+//            System.out.println("Cluster purity = " + P.callPurity(bestPopulation.getClusterResult()));
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(clustering.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex) {
+//            Logger.getLogger(clustering.class.getName()).log(Level.SEVERE, null, ex);
+//        }          
+        System.out.println("Fitness " + bestPopulation.getFitness());
+
           //aGenetic.printAllChromosomes();
-          
-          //aGenetic.printAllChromosomes();
-          AGAC.outputText.append("\n==== Finished====\n");
-          AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length()-1);
-          
-          java.util.Date end_time= new java.util.Date();
-          
-          System.out.println("Start: " + new Timestamp(start_time.getTime()));
-          System.out.println("End: " + new Timestamp(end_time.getTime()));
-          
-          
-          
-          AGAC.outputText.append("Start: " + new Timestamp(start_time.getTime()) + "\n");
-          AGAC.outputText.append("End: " + new Timestamp(end_time.getTime()) + "\n");
-          AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length()-1);
-          Double secs = new Double((end_time.getTime() - start_time.getTime()) * 0.001);
-          AGAC.outputText.append("Time processed: " + secs + " Sec.\n");
-          AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length()-1);
-          
-          
-          
-          
-          JOptionPane.showMessageDialog(null, "Done" );
-          
+        //aGenetic.printAllChromosomes();
+        AGAC.outputText.append("\n==== Finished====\n");
+        AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length() - 1);
+
+        java.util.Date end_time = new java.util.Date();
+
+        System.out.println("Start: " + new Timestamp(start_time.getTime()));
+        System.out.println("End: " + new Timestamp(end_time.getTime()));
+
+        AGAC.outputText.append("Start: " + new Timestamp(start_time.getTime()) + "\n");
+        AGAC.outputText.append("End: " + new Timestamp(end_time.getTime()) + "\n");
+        AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length() - 1);
+        Double secs = new Double((end_time.getTime() - start_time.getTime()) * 0.001);
+        AGAC.outputText.append("Time processed: " + secs + " Sec.\n");
+        AGAC.outputText.setCaretPosition(AGAC.outputText.getText().length() - 1);
+
+        JOptionPane.showMessageDialog(null, "Done");
+
     }
 }
